f68dba8a3b2a8b0de483d9f08bda9325a779b8e4
Ayalamri <ayalamrioui19@gmail.com>
2023-06-15 19:36:47 -0700

    n

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
  *
  * Description: C
  *
- * Return: always 0
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-printf("Siez of char: %lu byte(s)\n", sizeof(char));
-printf("Siez of int: %lu byte(s)\n", sizeof(int));
-printf("Siez of long int: %lu byte(s)\n", sizeof(long int));
-printf("Siez of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Siez of float: %lu byte(s)\n", sizeof(float));
+printf("Siez of char: %d byte(s)\n", sizeof(char));
+printf("Siez of int: %d byte(s)\n", sizeof(int));
+printf("Siez of long int: %d byte(s)\n", sizeof(long int));
+printf("Siez of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Siez of float: %d byte(s)\n", sizeof(float));
 return (0);
 }
