46aa55db095a9c436a69c958e1ca55846cefd0be
Ayalamri <ayalamrioui19@gmail.com>
2023-06-15 18:50:01 -0700

    y

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-/**
- * main - entry point
- *
- * Description: C
- *
- * Return: always 0
+/*i*
+* main - entry point
+*
+* Description: C
+*
+* Return: always 0
 */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
