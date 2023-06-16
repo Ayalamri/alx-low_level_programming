7ebb4b9c4c96c06c7789c2768af9ecf4d740be57
Ayalamri <ayalamrioui19@gmail.com>
2023-06-16 11:25:23 -0700

    ghd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 /**
  * main - entry point
  *
- * Description: Random number
+ * Description: false or true validation
  *
- * Return : 0 (Success)
+ * Return : 0 (success)
 */
 
 int main(void)
@@ -17,10 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive", n);
+		printf("%i is positive\n", n);
 	else if (n < 0)
-		printf("%i is negative", n);
+		printf("%i is negative\n", n);
 	else
-		printf("%i is zero", n);
+		printf("%i is zero\n", n);
 	return (0);
 }
