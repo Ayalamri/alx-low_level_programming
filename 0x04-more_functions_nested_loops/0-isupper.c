ee7d4b9c2b69f0ce5d243517181d4f1a40857792
Ayalamri <ayalamrioui19@gmail.com>
2023-06-22 07:47:30 -0700

    hb

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include"main.h"
 
 /**
- * _islower - function tells if a letter is lowercase or not
+ * _isupper - function tells if a letter is upper
  *
- * @c: checks the input of function
+ * @c: input for alphabet
  *
- * Return: return 1 if lowercase otherwise always 0
+ * Return: return 1 if uppercase otherwise always 0
 */
 
 int _islower(int c)
 {
-        if (c  >= 97 && c <= 122)
+        if (c  >= 65 && c <= 90)
                 return (1);
         return (0);
 }
