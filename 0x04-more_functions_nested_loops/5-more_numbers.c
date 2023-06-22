5aeee5669999e98c2515de8e29a6c282286c1a9f
Ayalamri <ayalamrioui19@gmail.com>
2023-06-22 15:15:10 -0700

    bhjh

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,6 @@
 /**
  * more_numbers - Prints numbers from 0 to 14, 10 times
  */
-
 void more_numbers(void)
 {
 	int i, j;
@@ -12,7 +11,7 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
