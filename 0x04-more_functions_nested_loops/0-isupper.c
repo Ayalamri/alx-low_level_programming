9298bd98822577b875e2b2a6d0a8d98bcc05a95d
Ayalamri <ayalamrioui19@gmail.com>
2023-06-22 07:43:46 -0700

    hgvg

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _islower - function tells if a letter is lowercase or not
+ *
+ * @c: checks the input of function
+ *
+ * Return: return 1 if lowercase otherwise always 0
+*/
+
+int _islower(int c)
+{
+        if (c  >= 97 && c <= 122)
+                return (1);
+        return (0);
+}
