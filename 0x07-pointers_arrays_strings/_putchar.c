5f81ce37514d0a81e75cbc76b251e236c4324288
Ayalamri <ayalamrioui19@gmail.com>
2023-07-03 16:35:06 -0700

    gj

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c
+ * @c: the character to print
+ *
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+        write(1, &c, 1);
+
+        return (0);
+}
