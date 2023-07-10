72d5c003307aee08a899ab633bc94880b8eef25c
Ayalamri <ayalamrioui19@gmail.com>
2023-07-10 09:26:45 -0700

    cc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 	lens1 = strlen(s1);
 	lens2 = strlen(s2);
 
-	ptr = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	ptr = (char *)malloc(sizeof(char) * (lens1 + lens2 + 1));
 
 	if (ptr == NULL)
 		return (NULL);
