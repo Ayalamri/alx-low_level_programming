c013138d81e00f9d6bfed065c90d9eada460bb25
Ayalamri <ayalamrioui19@gmail.com>
2023-08-01 14:06:24 -0700

    eeee

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,33 +1,28 @@
 #include"lists.h"
+#include <stddef.h>
 
 /**
  * reverse_listint - Reverses a listint_t linked list.
  * @head: Pointer to a pointer to the head node of the list.
- *
- * This function reverses the given linked list in-place by reversing the order
- * of the nodes. It does not use malloc, free, or arrays, and uses a maximum of
- * two variables for the operation. It returns a pointer to the first node of
- * the reversed list.
- *
  * Return: A pointer to the first node of the reversed list.
  */
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *x = NULL;
+	listint_t *pr = NULL;
 
-	listint_t *c = *head;
+	listint_t *cur = *head;
 
 	listint_t *next;
 
-	while (c != NULL)
+	while (cur != NULL)
 	{
-		next = c->next;
-		c->next = x;
-		x = c;
-		c = next;
-	};
+		next = cur->next;
+		cur->next = pr;
+		pr = cur;
+		cur = next;
+	}
 
-	*head = x;
+	*head = pr;
 	return (*head);
 }
