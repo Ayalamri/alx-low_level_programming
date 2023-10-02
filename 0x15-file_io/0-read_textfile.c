05b369cc08ddace1f7d7470261d3271110689724
Ayalamri <ayalamrioui19@gmail.com>
2023-10-02 16:54:47 -0700

    drdvg

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,38 +10,17 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-		return (0);
-
 	int x;
-	ssize_t r, w;
-	char *y;
+	ssize_t y;
+	char bouf[BUF_SIZE * 8];
 
+	if (!filename || !letters)
+		return (0);
 	x = open(filename, O_RDONLY);
 	if (x == -1)
 		return (0);
-
-	y = malloc(sizeof(char) * letters);
-	if (y == NULL)
-	{
-		close(x);
-		return (0);
-	}
-
-	r = read(x, y, letters);
+	y = read(x, &bouf[0], letters);
+	y = write(STDOUT_FILENO, &bouf[0], y);
 	close(x);
-
-	if (r == -1)
-	{
-		free(y);
-		return (0);
-	}
-
-	w = write(STDOUT_FILENO, y, r);
-	free(y);
-
-	if (w == -1 || (size_t)w < r)
-		return (0);
-
-	return (w);
+	return (y);
 }
