f31451dd194272a2e1418218771576bf2427d0d9
Ayalamri <ayalamrioui19@gmail.com>
2023-12-21 16:19:51 -0800

    rttyu

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,57 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: the key (cannot be an empty string)
+ * @value: the value associated with the key (can be an empty string)
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, add the new node at the beginning of the list
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    hash_node_t *new_node, *head;
+    unsigned long int index;
+
+    if (ht == NULL || key == NULL || *key == '\0')
+        return (0);
+
+    index = key_index((unsigned char *)key, ht->size);
+
+    head = ht->array[index];
+
+    while (head)
+    {
+        if (strcmp(head->key, key) == 0)
+        {
+            /* Key already exists, update value */
+            free(head->value);
+            head->value = strdup(value);
+            if (head->value == NULL)
+                return (0);
+            return (1);
+        }
+        head = head->next;
+    }
+
+    /* Key doesn't exist, create a new node and add to the beginning of the list */
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
+
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    if (new_node->key == NULL || new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node->value);
+        free(new_node);
+        return (0);
+    }
+
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    return (1);
+}
